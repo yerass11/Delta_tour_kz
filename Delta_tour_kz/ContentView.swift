f3cb7cabfc5112fbcb724ff0e989af04e5d7744e
yerass11 <y_saiman@kbtu.kz>
2024-10-23 20:30:27 +0500

    Initial Commit

---
+++ Delta_tour_kz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Delta_tour_kz
+//
+//  Created by Yerasyl on 23.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Delta_tour_kz/Delta_tour_kzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delta_tour_kzApp.swift
+//  Delta_tour_kz
+//
+//  Created by Yerasyl on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Delta_tour_kzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
